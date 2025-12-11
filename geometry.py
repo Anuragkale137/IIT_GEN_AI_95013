057d4c9f6382811bcb2ca4cad227615e54db09ec
Anuragkale <anuragkale264@gmail.com>
2025-12-11 09:59:22 +0530

    commit

---
+++ geometry.py
@@ -0,0 +1,9 @@
+
+
+def cal_rect_area(len,br):
+    print("Area of the rectangle is :",len*br)
+
+def cal_rect_peri(len,br):
+    print("perimeter of the rectanle is :",2*(len+br))
+
+    
--- main.py
+++ main.py
@@ -5,3 +5,11 @@ num1 = int(input("Enter first number:"))
 num2 = int(input("enter the num2 :"))
 arith.addition(num1,num2)
 arith.substraction(num1,num2)
+
+import geometry as geo
+length = int(input("Enter lenght of the rectanle :"))
+breadth = int(input("Enter the breadth of the rectanle :"))
+
+geo.cal_rect_area(length,breadth)
+geo.cal_rect_peri(length,breadth)
+
