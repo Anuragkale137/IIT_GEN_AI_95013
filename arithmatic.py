f52cc329146d697e69574024d4de077d24bcbea0
Anuragkale <anuragkale264@gmail.com>
2025-12-11 09:42:18 +0530

    Arithmatic

---
+++ arithmatic.py
@@ -0,0 +1,6 @@
+
+def addition(num1,num2):
+    print("Sum of the num1 and num2 is :",num1+num2)
+
+def substraction(num1,num2):
+    print("substraction of num1 and num2 :",num1-num2)
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("hello world")
+
+import arithmatic as arith
+num1 = int(input("Enter first number:"))
+num2 = int(input("enter the num2 :"))
+arith.addition(num1,num2)
+arith.substraction(num1,num2)
